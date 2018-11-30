4b031bc871f28b327be2866256fe6fa7dd9035e3
rytwalker <rytwalker@gmail.com>
2018-11-30 11:36:59 -0500

    Added login route

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const jwtSecret = require('../_secrets/keys').jwtKey;
 
 const db = require('../database/dbConfig');
 
@@ -10,6 +12,18 @@ module.exports = server => {
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
 };
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username
+  };
+  const secret = jwtSecret;
+  const options = {
+    expiresIn: '5m'
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 async function register(req, res) {
   // implement user registration
@@ -32,15 +46,36 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  const loginData = req.body;
+
+  if (!loginData.username || !loginData.password) {
+    return res
+      .status(400)
+      .json({ message: 'Username and password are required.' });
+  }
+
+  try {
+    const user = await db('users')
+      .where({ username: loginData.username })
+      .first();
+    if (user && bcrypt.compareSync(loginData.password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: `Welcome ${user.username}`, token });
+    } else {
+      res.status(401).json({ message: 'That password does not match.' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Something went wrong logging in to get dad jokes.' });
+  }
 }
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get('https://safe-falls-22549.herokuapp.com/random_ten')
     .then(response => {
       res.status(200).json(response.data);
     })
